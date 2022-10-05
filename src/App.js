d351e763a3ec523a42c3b24cea488bcbc561aa62
Afrizal Fathan <rieslinger8@gmail.com>
2022-10-05 11:50:07 +0700

    add localstorage

--- src/App.js
+++ src/App.js
@@ -1,65 +1,79 @@
-import Nav from "./components/Nav";
-import "./style/style.css";
-import { React, useState } from "react";
-import { Button, Modal, Form, Table } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
-import DataTable from "./components/DataTable";
+import Nav from "./components/Nav"
+import "./style/style.css"
+import { React, useState } from "react"
+import { Button, Modal, Form, Table } from "react-bootstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons"
+import DataTable from "./components/DataTable"
+import { useEffect } from "react"
 
 function App() {
-    const [nama, setNama] = useState("");
-    const [npm, setNpm] = useState("");
-    const [alamat, setAlamat] = useState("");
-    const [telepon, setTelepon] = useState("");
-    const [edit, setEdit] = useState("");
-    const [mahasiswa, setMahasiswa] = useState([]);
-    const [nomor, setNomor] = useState(1);
-    const [show, setShow] = useState(false);
-    const [tambah, setTambah] = useState("Tambah Data");
-    const [validated, setValidated] = useState(false);
-    const [errors, setErrors] = useState({});
+    useEffect(() => {
+        localStorage.setItem("data", JSON.stringify(mahasiswa))
+    })
 
-    const handleShow = () => setShow(true);
-    const handleClose = () => setShow(false);
+    function getDataFromLocalStorage() {
+        const dataMhs = localStorage.getItem("data")
+        if (dataMhs) {
+            return JSON.parse(dataMhs)
+        } else {
+            return []
+        }
+    }
+
+    const [nama, setNama] = useState("")
+    const [npm, setNpm] = useState("")
+    const [alamat, setAlamat] = useState("")
+    const [telepon, setTelepon] = useState("")
+    const [edit, setEdit] = useState("")
+    const [mahasiswa, setMahasiswa] = useState(getDataFromLocalStorage())
+    const [nomor, setNomor] = useState(1)
+    const [show, setShow] = useState(false)
+    const [tambah, setTambah] = useState("Tambah Data")
+    const [validated, setValidated] = useState(false)
+    const [errors, setErrors] = useState({})
+
+    const handleShow = () => setShow(true)
+    const handleClose = () => setShow(false)
 
     // Fungsi menghapus data
     function deleteHandler(siswaId) {
         const filteredSiswa = mahasiswa.filter(
             (siswa) => siswa.id !== siswaId,
             setNomor(nomor - 1)
-        );
-        setMahasiswa(filteredSiswa);
+        )
+        setMahasiswa(filteredSiswa)
     }
 
     // fungsi untuk memasukan data dalam field untuk pengecekan form
     function findFormErrors() {
-        const newErrors = {};
-        if (!nama || nama === "") newErrors.nama = "Nama tidak boleh kosong!";
-        if (!npm || npm === "") newErrors.npm = "NPM tidak boleh kosong!";
+        const newErrors = {}
+        if (!nama || nama === "") newErrors.nama = "Nama tidak boleh kosong!"
+        if (!npm || npm === "") newErrors.npm = "NPM tidak boleh kosong!"
         if (!telepon || telepon === "")
-            newErrors.telepon = "Telepon tidak boleh kosong!";
+            newErrors.telepon = "Telepon tidak boleh kosong!"
         if (!alamat || alamat === "")
-            newErrors.alamat = "Alamat tidak boleh kosong!";
+            newErrors.alamat = "Alamat tidak boleh kosong!"
 
-        return newErrors;
+        return newErrors
     }
 
     // Fungsi untuk mengerate data key
     function generateKey() {
-        return Date.now();
+        return Date.now()
     }
 
     //fungsi menyimpan data
     function saveData(e) {
-        e.preventDefault();
-        const newErrors = findFormErrors();
+        e.preventDefault()
+        const newErrors = findFormErrors()
 
         // kondisi jika suatu form kosong
         if (Object.keys(newErrors).length > 0) {
-            setErrors(newErrors);
-            return;
+            setErrors(newErrors)
+            return
         } else {
-            setValidated(true);
+            setValidated(true)
             if (edit.id) {
                 const newData = {
                     id: edit.id,
@@ -67,18 +81,18 @@ function App() {
                     npm,
                     alamat,
                     telepon,
-                };
+                }
 
                 const editSiswaIndex = mahasiswa.findIndex(
                     (siswa) => siswa.id === edit.id
-                );
+                )
 
-                const updatedData = [...mahasiswa];
+                const updatedData = [...mahasiswa]
 
-                updatedData[editSiswaIndex] = newData;
-                setMahasiswa(updatedData);
+                updatedData[editSiswaIndex] = newData
+                setMahasiswa(updatedData)
 
-                return cancelEditHandler();
+                return cancelEditHandler()
             }
 
             setMahasiswa([
@@ -90,47 +104,47 @@ function App() {
                     alamat,
                     telepon,
                 },
-            ]);
-            setTambah("Tambah Data");
-            setNama("");
-            setNpm("");
-            setAlamat("");
-            setTelepon("");
-            handleClose();
+            ])
+            setTambah("Tambah Data")
+            setNama("")
+            setNpm("")
+            setAlamat("")
+            setTelepon("")
+            handleClose()
         }
     }
 
     // fungsi jika tombol tambah diklik
     function handleModalShow() {
-        handleShow();
-        setTambah("Tambah Data");
-        setNama("");
-        setNpm("");
-        setAlamat("");
-        setTelepon("");
+        handleShow()
+        setTambah("Tambah Data")
+        setNama("")
+        setNpm("")
+        setAlamat("")
+        setTelepon("")
     }
 
     // fungsi jika tombol edit diklik
 
     function editHandler(siswa) {
-        handleShow();
-        setEdit(siswa);
-        setTambah("Edit Data");
-        setNama(siswa.nama);
-        setNpm(siswa.npm);
-        setAlamat(siswa.alamat);
-        setTelepon(siswa.telepon);
+        handleShow()
+        setEdit(siswa)
+        setTambah("Edit Data")
+        setNama(siswa.nama)
+        setNpm(siswa.npm)
+        setAlamat(siswa.alamat)
+        setTelepon(siswa.telepon)
     }
 
     // fungsi jika tombol close edit diklik
 
     function cancelEditHandler() {
-        setEdit({});
-        setNama("");
-        setNpm("");
-        setAlamat("");
-        setTelepon("");
-        handleClose();
+        setEdit({})
+        setNama("")
+        setNpm("")
+        setAlamat("")
+        setTelepon("")
+        handleClose()
     }
 
     return (
@@ -248,7 +262,7 @@ function App() {
                 ))}
             </Table>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
